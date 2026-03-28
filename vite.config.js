1f5999473ad971a86117ab5611266bee41ea76e7
Fabricio Fiss Bartz <fabriciofissbartz@gmail.com>
2026-03-28 16:21:34 -0300

    Deploy: gerando build na pasta docs

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Calculadora_de_Insumos_Agricolas/',
+  build: {
+    outDir: 'docs', 
+  }
 })
